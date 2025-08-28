0a7f3588186facddf18be3eafe4524d3f6c1af37
amalik19 <113020717+amalik19@users.noreply.github.com>
2025-08-28 18:59:24 +0000

    1) Added login connectivity app file 2) Configure sqlite connections 3) Added login html page

---
+++ login/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20),unique = True, nullable = False)
+    password = db.Column(db.String(50), nullable = False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators={InputRequired(), Length(min=4, max=20),}, render_kw={"placeholder": "Username"})
+    password = PasswordField(validators={InputRequired(), Length(min=4, max=20),}, render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please choose a different one.")
+        
+class LoginForm(FlaskForm):
+    username = StringField(validators={InputRequired(), Length(min=4, max=20),}, render_kw={"placeholder": "Username"})
+    password = PasswordField(validators={InputRequired(), Length(min=4, max=20),}, render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password = hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
