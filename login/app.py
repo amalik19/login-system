e7b7328ac2e683bd1e00edd505ba1e239eae7cd8
amalik19 <113020717+amalik19@users.noreply.github.com>
2025-11-17 09:23:57 +0000

    Completed the login system, everything is now working as it should.

--- login/app.py
+++ login/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -8,14 +8,20 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-# seperate below configuration in json file and load here from that json file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-# seperate each class into its own file
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20),unique = True, nullable = False)
@@ -44,7 +50,24 @@ def home():
 @app.route('/login', methods = ["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods = ["GET", "POST"])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods = ["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/register', methods = ["GET", "POST"])
 def register():
